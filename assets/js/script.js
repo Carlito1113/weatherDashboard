08eb3c250a1d64dc64ae65ca60969c2cfeb6426b
Carlito Algarin <19lito96@gmail.com>
2020-09-21 18:07:01 -0400

    added jumbotron with some styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,39 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-$("#weather-btn").on("click", function() {
+$("#weather-btn").on("click", function(event) {
     event.preventDefault();
 
    var cityInput = $("#weather-input").val().trim();
-    
+    console.log(cityInput);
+    currentWeather(cityInput);
+    forecast(cityInput);
 
 
 })
 
-function currentWeather() {
+function currentWeather(city) {
 
     $.ajax({
         method="GET",
-        url:,
+        url:"https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2b082fc72e5c1ea2495f67dc43747872&units=imperial"
         
-    }).than(function(data){
+    }).then(function(data){
 
-
+        console.log(data);
 
     })
-
-
-
 }
+function forecast(city) {
 
+    $.ajax({
+        method: "GET",
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=2b082fc72e5c1ea2495f67dc43747872&units=imperial"
+    }).then(function (data){
+        console.log(data);
+    })
+}
 
 
 
