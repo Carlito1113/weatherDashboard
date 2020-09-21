8efbba7c747457b6e4ff3cc33f56374c3cda41ae
Carlito Algarin <19lito96@gmail.com>
2020-09-21 14:59:59 -0400

    homework startup activity steps

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function() {
+
+
+$("#weather-btn").on("click", function() {
+    event.preventDefault();
+
+   var cityInput = $("#weather-input").val().trim();
+    
+
+
+})
+
+function currentWeather() {
+
+    $.ajax({
+        method="GET",
+        url:,
+        
+    }).than(function(data){
+
+
+
+    })
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
